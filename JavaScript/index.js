c1384562aab8e93a5018f701cf3604ab6597da00
harshals21 <61793912+harshals21@users.noreply.github.com>
2022-08-03 12:43:22 +0530

    JavaScript basics

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,2 +1,65 @@
 // This is my first JavaScript code!
-console.log("Hello World");
+// console.log("Hello World");
+
+// variables
+// let name = "Harsh";
+// console.log(name);
+
+// Cannot be a reserved keyword
+// Should be meaningful
+// Cannot start with a number (1name)
+// Cannot contain a space or hyphen (-)
+// Are case-sensitive
+
+// let firstName = "Abc";
+// let lastName = "Xyz";
+
+// const interestRate = 0.3;
+// interestRate = 1;
+// console.log(interestRate);
+
+// If you need to re-assign use let otherwise const
+
+// let name = "Mosh"; // String Literal
+// let age = 30; // Number Literal
+// let isApproved = false; // Boolean Literal
+// let firstName = undefined;
+// let selectedColor = null;
+
+// Objects
+
+// let person = {
+//   name: "Mosh",
+//   age: 30,
+// };
+
+// Dot Notation
+// person.name = "John";
+
+// Bracket Notation
+// let selection = "name";
+// person[selection] = "Mary";
+
+// console.log(person.name);
+
+// Arrays
+// In JavaScript, array can be of multiple types
+
+// let selectedColors = ["red", "blue"];
+// selectedColors[2] = 1;
+// console.log(selectedColors.length);
+
+// Functions
+
+// Performing a task
+// function greet(name, lastName) {
+//   console.log("Hello " + name + ' ' + lastName);
+// }
+
+// Calculating a value
+// function square(number) {
+//   return number * number;
+// }
+
+// greet('John', 'Smith');
+// console.log(square(2));
