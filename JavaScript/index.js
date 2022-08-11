5e1b4966dc994380556557d5163c08405f316d4e
harshals21 <61793912+harshals21@users.noreply.github.com>
2022-08-11 21:35:19 +0530

    Array methods

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -365,16 +365,69 @@ checks value (Lose equality)
 
 // JOINING ARRAYS
 
-const numbers = [1, 2, 3];
-const joined = numbers.join(',');
-console.log(joined);
+// const numbers = [1, 2, 3];
+// const joined = numbers.join(',');
+// console.log(joined);
 
-const message = 'This is my first message';
-const parts = message.split(' ');
-console.log(parts);
+// const message = 'This is my first message';
+// const parts = message.split(' ');
+// console.log(parts);
 
-const combined = parts.join('-');
-console.log(combined);
+// const combined = parts.join('-');
+// console.log(combined);
+
+// SORTING ARRAYS
+// const numbers = [2, 3, 1];
+// numbers.sort();
+// console.log(numbers);
+
+// numbers.reverse();
+// console.log(numbers);
+
+// const courses = [
+//     { id: 1, name: 'Node.js' },
+//     { id: 2, name: 'javaScript' },
+// ];
+
+// courses.sort(function (a, b) {
+//     // a < b => -1
+//     // a > b => 1
+//     // a === b => 0
+
+//     const nameA = a.name.toLowerCase();
+//     const nameB = b.name.toLowerCase();
+
+//     if (nameA < nameB) return -1;
+//     if (nameA > nameB) return 1;
+//     return 0;
+// });
+
+// console.log(courses);
+
+// TESTING THE ELEMENTS OF AN ARRAY
+
+// const numbers = [1, -1, 2, 3];
+
+// // every()
+// // some()
+
+// const atLeastOnePositive = numbers.some(function (value) {
+//     return value >= 0;
+// });
+
+// console.log(atLeastOnePositive);
+
+// FILTERING AN ARRAY
+
+// const numbers = [1, -1, 2, 3];
+
+// const filtered = numbers.filter(n => n >= 0);
+
+// console.log(filtered);
+
+// Mapping an array
+
+
 
 
 
